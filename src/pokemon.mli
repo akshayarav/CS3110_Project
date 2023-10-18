0c155089a46c45f25dbc5eb23e2d9608c0295d81
Akshay Aravind <akshayarav2013@gmail.com>
2023-10-18 15:11:45 -0400

    Implement Pokemon.attack

--- src/pokemon.mli
+++ src/pokemon.mli
@@ -1,28 +1,20 @@
 (** Type representing the various Pokemon types. *)
-type ptype = 
-  | Fire
-  | Water
-  | Grass
-  | Electric
-  (* Add other types as needed *)
+type ptype
 
 (** Type representing a move a Pokemon can perform. *)
-type move = {
-  name: string;
-  damage: int;
-  ptype: ptype;
-}
+type move 
 
 (** A pokemon representation. *)
-module type Pokemon = sig
-  type t = {
-    name : string;
-    ptype : string;
-    hp : int;
-    moves : move list;
-  }
-  (** Stats of the Pokemon *)
+type t = {
+  name : string;
+  ptype : string;
+  hp : int;
+  moves : move list;
+}
+(** Stats of the Pokemon *)
 
-  val create : string -> string -> int -> move list -> t
-  (** Create a pokemon *)
-end
+val create : string -> string -> int -> move list -> t
+(** Create a pokemon *)
+
+val attack : move -> t -> t
+(** A move is applied onto a Pokemon and the Pokemon's hp is updated*)
