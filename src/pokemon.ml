d9a3bc6e0ea2c1d7c4b69095278236167291b449
Akshay Aravind <aa2288@cornell.edu>
2023-10-22 16:17:49 -0400

    Change Pokemon.attack to update the pokemon state to feint = true if the pokemon feinted

--- src/pokemon.ml
+++ src/pokemon.ml
@@ -15,16 +15,20 @@ type pokemon = {
   hp : int;
   moves : move list;
   level : int;
-  xp: (int*int);
-  faint: bool;
+  xp : int * int;
+  faint : bool;
 }
 (** Type for a pokemon has several stats *)
 
 (** Creates a new pokemon *)
-let create name ptype hp moves level = { name; ptype; hp; moves; level; xp = (0, 10); faint = false }
+let create name ptype hp moves level =
+  { name; ptype; hp; moves; level; xp = (0, 10); faint = false }
 
 (** Pokemon's hp is updated when attacked *)
-let attack move pokemon = { pokemon with hp = pokemon.hp - move.damage }
+let attack move pokemon =
+  let new_hp = pokemon.hp - move.damage in
+  if new_hp <= 0 then { pokemon with hp = 0; faint = true }
+  else { pokemon with hp = new_hp }
 
 (** Prints out pokemon and its stats *)
 let to_string pokemon = pokemon.name ^ ": " ^ ptype_to_string pokemon.ptype
@@ -35,11 +39,12 @@ let name pokemon = pokemon.name
 (*Checks if enough xp to level up and updates accordingly*)
 let level_up pokemon =
   let diff = fst pokemon.xp - snd pokemon.xp in
-    if diff >= 0 then { pokemon with xp = (diff, snd pokemon.xp + 20)}
-    else pokemon
+  if diff >= 0 then { pokemon with xp = (diff, snd pokemon.xp + 20) }
+  else pokemon
 
 (* Adds XP gained in victory *)
-let add_xp pokemon xp_earned = 
-  let updated_pokemon = 
-    { pokemon with xp = (fst pokemon.xp + xp_earned, snd pokemon.xp)} in
+let add_xp pokemon xp_earned =
+  let updated_pokemon =
+    { pokemon with xp = (fst pokemon.xp + xp_earned, snd pokemon.xp) }
+  in
   level_up updated_pokemon
