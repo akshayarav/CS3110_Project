88285e66720391be8deffd9c71ad2f152b60d5d3
Abhi <ahv37@cornell.edu>
2023-10-21 23:45:27 -0400

    Implemented Battle functionality in main.ml pokemon can now battle each other

--- src/main.ml
+++ src/main.ml
@@ -3,6 +3,7 @@ open Printf
 open Pokemon
 open Pokedex
 open Player
+open Ai
 (* open Battle *)
 
 (* Menu options *)
@@ -46,34 +47,73 @@ and choose_starter () =
       choose_starter ()
 
 (* Simulate a battle with a wild pokemon *)
-(* Simulate a battle with a wild pokemon *)
 and wild_battle player =
-  match player.current_pokemon with
-  | Some pokemon ->
-      let moves = pokemon.moves in
-      printf "Choose a move for %s:\n" (Pokemon.name pokemon);
+  let opponent_index = Random.int (List.length wild_pokemon) in
+  let opponent = List.nth wild_pokemon opponent_index in
+  let player_pokemon = match player.current_pokemon with
+    | Some p -> p
+    | None -> failwith "No current Pokémon to battle with"
+  in
+  printf "A wild %s appears!\n" (Pokemon.name opponent);
+  
+  let rec battle_loop player_pokemon opponent =
+    if player_pokemon.hp <= 0 then (
+      printf "Your %s fainted. You lost the battle!\n" player_pokemon.name;
+      display_menu player
+    )
+    else if opponent.hp <= 0 then (
+      printf "You defeated the wild %s! You won the battle!\n" (Pokemon.name opponent);
+      display_menu player
+    )
+    else (
+      printf "\nYour %s's HP: %d\n" player_pokemon.name player_pokemon.hp;
+      printf "Wild %s's HP: %d\n" (Pokemon.name opponent) opponent.hp;
+      
+      (* Player's turn *)
+      printf "Choose a move for %s:\n" player_pokemon.name;
       List.iteri
         (fun i (move : move) ->
           printf "%d. %s (Type: %s, Damage: %d)\n" (i + 1) move.name
             (ptype_to_string move.ptype)
             move.damage)
-        moves;
+        player_pokemon.moves;
+      
       let rec get_move_choice () =
         try
           let choice = read_int () in
-          if choice >= 1 && choice <= List.length moves then
-            let chosen_move = List.nth moves (choice - 1) in
-            printf "You chose %s!\n" chosen_move.name
+          if choice >= 1 && choice <= List.length player_pokemon.moves then
+            let chosen_move = List.nth player_pokemon.moves (choice - 1) in
+            printf "You chose %s!\n" chosen_move.name;
+            let updated_opponent = Pokemon.attack chosen_move opponent in
+            if updated_opponent.hp <= 0 then (
+              printf "You defeated the wild %s! You won the battle!\n" (Pokemon.name opponent);
+              display_menu player
+            )
+            else (
+              let ai_move = choose_move opponent in
+              printf "Wild %s chose %s!\n" (Pokemon.name opponent) ai_move.name;
+              let updated_player_pokemon = Pokemon.attack ai_move player_pokemon in
+              if updated_player_pokemon.hp <= 0 then (
+                printf "Your %s fainted. You lost the battle!\n" player_pokemon.name;
+                display_menu player
+              )
+              else (
+                battle_loop updated_player_pokemon updated_opponent
+              )
+            )
           else (
             printf "Invalid move choice. Please enter a valid number: ";
-            get_move_choice ())
-        with Failure _ ->
-          printf "Invalid input. Please enter a valid number: ";
-          get_move_choice ()
+            get_move_choice ()
+          )
+        with
+        | Failure _ ->
+            printf "Invalid input. Please enter a valid number: ";
+            get_move_choice ()
       in
-      get_move_choice ();
-      display_menu player
-  | None ->
-      printf "No current Pokémon to choose a move for\n";
-      display_menu player
+      get_move_choice ()
+    )
+  in
+  battle_loop player_pokemon opponent
+
+
 let () = display_menu player
