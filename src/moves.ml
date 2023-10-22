1c0e6fe293b9cf15be065cec40328786efa508e3
Akshay Aravind <akshayarav2013@gmail.com>
2023-10-21 20:14:52 -0400

    Initial moves list

---
+++ src/moves.ml
@@ -0,0 +1,6 @@
+open Pokemon
+
+let tackle = { name = "Tackle"; damage = 10; ptype = Normal }
+let growl = { name = "Growl"; damage = 0; ptype = Normal }
+let scratch = { name = "Scratch"; damage = 10; ptype = Normal }
+let tail_whip = { name = "Tail Whip"; damage = 0; ptype = Normal }
