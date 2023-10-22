23b47c1d124e6d758e5c6f020f00e51ad4dc3005
Akshay Aravind <aa2288@cornell.edu>
2023-10-21 21:10:51 -0400

    Add documentation for Player and Pokemon

--- src/pokemon.ml
+++ src/pokemon.ml
@@ -9,6 +9,8 @@ let string_to_ptype = function
   | Grass -> "Grass Type"
   | _ -> failwith "Unknown Type"
 
+
+(** Type for a pokemon has several stats*)  
 type pokemon = {
   name : string;
   ptype : ptype;
@@ -17,7 +19,11 @@ type pokemon = {
   level : int;
 }
 
+(** Creates a new pokemon*)
 let create name ptype hp moves level = { name; ptype; hp; moves; level }
+(** Pokemon's hp is updated when attacked*)
 let attack move pokemon = { pokemon with hp = pokemon.hp - move.damage }
+(** Prints out pokemon and its stats*)
 let to_string pokemon = pokemon.name ^ ": " ^ string_to_ptype pokemon.ptype
+(** Prints out the pokemon's name*)
 let name pokemon = pokemon.name
