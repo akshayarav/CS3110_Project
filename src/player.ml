6f50af4b91efa992c96880c49650054ccc52be6b
Dan Wei Zuo <dz267@cornell.edu>
2023-10-21 22:44:04 -0400

    Add wild_battle to prompt choosing move
    - Expaneded on battle.ml and ai.ml

--- src/player.ml
+++ src/player.ml
@@ -1,11 +1,16 @@
 open Pokemon
 
-type player = { team : pokemon list; first_pokemon : pokemon option }
-(** A player has a team and a first pokemon to send out *)
+type player = {
+  team : pokemon list;
+  current_pokemon : pokemon option;
+}
+(** A player has a team, a first pokemon to send out, and the current pokemon in battle *)
 
 (** Creates a new player with an empty team*)
-let new_player = { team = []; first_pokemon = None }
+let new_player = { team = []; current_pokemon = None }
 
-(** Add a new pokemon to the players team*)
+(** Add a new pokemon to the player's team*)
 let add_team pokemon player =
-  { team = pokemon :: player.team; first_pokemon = Some pokemon }
+  match player.team with
+  | [] -> { team = [pokemon]; current_pokemon = Some pokemon }
+  | _ -> { player with team = pokemon :: player.team }
