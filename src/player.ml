0efd33dc3be5935a3eb2b5bf142b677c796f5705
Akshay Aravind <akshayarav2013@gmail.com>
2023-10-21 21:10:51 -0400

    Add documentation for Player and Pokemon

--- src/player.ml
+++ src/player.ml
@@ -1,8 +1,11 @@
 open Pokemon
 
 type player = { team : pokemon list; first_pokemon : pokemon option }
+(** A player has a team and a first pokemon to send out *)
 
+(** Creates a new player with an empty team*)
 let new_player = { team = []; first_pokemon = None }
 
+(** Add a new pokemon to the players team*)
 let add_team pokemon player =
   { team = pokemon :: player.team; first_pokemon = Some pokemon }
