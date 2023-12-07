401f5cd0f71b150b112fa82bb797d1a169da6b4f
Hyungjun <hc867@cornell.edu>
2023-12-07 17:09:23 -0500

    Implemented tests for pokemon initialization
    - Tested squirtle init

--- src/test.ml
+++ src/test.ml
@@ -2,21 +2,32 @@ open OUnit2
 open Pokemon
 open Pokedex
 
-
-
-
 let squirtle1 = Pokemon.create base_squirtle 10
 let charmander1 = Pokemon.create base_charmander 11
 
-
-let tests =
+let init_tests =
  "suite" >::: [
    "hp check" >::
-   (fun _ -> assert_equal ~msg:"hp check" squirtle1.hp 10);
-   "hp check" >::
-   (fun _ -> assert_equal ~msg:"hp check" squirtle1.level 10);
+   (fun _ -> assert_equal ~msg:"hp check" ~printer:string_of_int 44 squirtle1.hp);
+   "level check" >::
+   (fun _ -> assert_equal ~msg:"level check" ~printer:string_of_int 10 squirtle1.level);
+   "xp check" >::
+   (fun _ -> assert_equal ~msg:"xp check" ~printer:string_of_int 0 (fst squirtle1.xp));
+   "xp total check" >::
+   (fun _ -> assert_equal ~msg:"xp total check" ~printer:string_of_int 100 (snd squirtle1.xp));
+   "feint check" >::
+   (fun _ -> assert_equal ~msg:"feint check" ~printer:string_of_bool false squirtle1.feint);
+   "d_mult check" >::
+   (fun _ -> assert_equal ~msg:"d_mult check" ~printer:string_of_float 1. squirtle1.damage_modifier);
+   "base check" >::
+   (fun _ -> assert_equal ~msg:"base check" base_squirtle squirtle1.base);
  ]
 
-
+let battle_test =
+  "suite" >::: [
+    (* "hp check" >::
+    (fun _ -> assert_equal ~msg:"hp check" ~printer:string_of_int 44 33); *)
+  ]
 let () =
- run_test_tt_main tests
+ run_test_tt_main init_tests;
+ (* run_test_tt_main battle_test *)
