47d4185391a86815d09bf20a59e6deabc13883d6
Akshay Aravind <akshayarav2013@gmail.com>
2023-12-09 15:30:01 -0500

    UI working for battle
    - Created helper functions to clear container and close dialog box

--- main/ui.ml
+++ main/ui.ml
@@ -1,5 +1,5 @@
 open Mylib
-(* open Printf *)
+open Printf
 
 let main_menu : (GWindow.window option) ref = ref None
 
@@ -18,32 +18,40 @@ let update_ui_string dialog message =
 
   dialog#show ()
 
-let prev_moves: GObj.widget list ref = ref []
+let clear_container container = (
+  let children = container#all_children in
+  List.iter (fun widget ->
+    container#remove widget;
+  ) children; )
 
-let rec update_ui_moves dialog moves player_pokemon opponent = (
-  let rec remove_prev_moves = function 
-    | (widget:GObj.widget) :: t -> dialog#vbox#remove widget; remove_prev_moves t
-    | [] -> ()
-in
-  remove_prev_moves !prev_moves;
-  let result_ref = ref false in
+let create_ok_button dialog = (
+  let ok_button = GButton.button ~label:"OK" ~packing:dialog#vbox#add () in
+
+  ignore (
+    ok_button#connect#clicked ~callback:(fun () ->
+      dialog#destroy (); 
+    )
+  );)
+  
+
+let rec update_ui_moves dialog moves player_pokemon opponent result = (
+  clear_container dialog#vbox;
   List.iteri
-    (fun i (move : Pokemon.move) ->
-      let label_text =
-        Printf.sprintf "%d. %s (Type: %s, Damage: %d)\n" (i + 1) move.name
-          (Pokemon.ptype_to_string move.m_ptype)
-          move.damage
-      in
-      let button = GButton.button ~label:(label_text) ~packing:dialog#vbox#add () in
+    (fun i (move: Pokemon.move) ->
+      let label_text = Printf.sprintf "%d. %s (Type: %s, Damage: %d)\n" (i + 1) move.name
+        (Pokemon.ptype_to_string move.m_ptype)
+        move.damage in
+      let button = GButton.button ~label:label_text ~packing:dialog#vbox#add () in
       ignore (
         button#connect#clicked ~callback:(fun () ->
-            Battle.on_move_chosen (Some move) player_pokemon opponent (update_ui_string dialog) (update_ui_moves dialog moves player_pokemon opponent) result_ref;
+            Battle.on_move_chosen (Some move) player_pokemon opponent (update_ui_string dialog) (update_ui_moves dialog moves player_pokemon opponent result) result;
         )
       );
-      prev_moves := button#coerce :: !prev_moves
-    )
-    moves;
-  dialog#show ();)
+    ) 
+    moves;  (* This is the list being iterated over *)
+  dialog#show ();
+)
+
 
 let create_button label vbox callback =
   let button = GButton.button ~label ~packing:vbox#add () in
@@ -86,14 +94,6 @@ and player_stats () =
   dialog#show ();
 
 and wild_battle () = 
-  let dialog = GWindow.dialog
-  ~title:"Wild Battle"
-  ~parent:(get_main_menu()) 
-  ~width:700
-  ~height:600
-  ~destroy_with_parent:true
-  ~modal:true
-  () in
 
   let opponent_index = Random.int (List.length Pokedex.wild_pokemon_base) in
   let opponent = Pokemon.create_random (List.nth Pokedex.wild_pokemon_base opponent_index) 5 in
@@ -102,17 +102,57 @@ and wild_battle () =
     | Some p -> p
     | None -> failwith "No current Pokémon to battle with"
   in
+
+  let dialog = GWindow.dialog
+  ~title: (sprintf "Wild Battle with %s!!" opponent.base.name)
+  ~parent:(get_main_menu()) 
+  ~width:700
+  ~height:600
+  ~destroy_with_parent:true
+  ~modal:true
+  () in
+
   let label = GMisc.label ~text:(Printf.sprintf "A wild %s appears!" opponent.base.name) () in
   ignore (dialog#vbox#add (label :> GObj.widget));
 
-  let button = GButton.button ~label:"OK" ~packing:dialog#action_area#add () in
-  ignore (button#connect#clicked ~callback:(fun () -> dialog#destroy ()));
-
   dialog#show ();
 
-  update_ui_moves dialog player_pokemon.base.moves player_pokemon opponent;
+  update_ui_moves dialog player_pokemon.base.moves player_pokemon opponent (on_wild_result dialog player_pokemon opponent) ;
 
-  display_menu ();
+
+and on_wild_result dialog player_pokemon opponent won = 
+  if won then (
+    let reward =
+      let min_reward = 1 in
+      let max_reward = 5 in
+      let earned_coins = min_reward + Random.int (max_reward - min_reward + 1) in
+      earned_coins
+    in
+    player := Player.adjust_coins reward !player;
+    update_ui_string dialog (sprintf "You also won %d coins!\n" reward);
+    let xp_gained = Pokemon.calculate_xp_gained opponent.level in
+    let updated_pokemon = Pokemon.add_xp player_pokemon xp_gained in
+    update_ui_string dialog (sprintf "Your %s gained %d XP.\n" updated_pokemon.base.name xp_gained);
+
+    (* TODO: ADD LEVEL UP AND EVOLUTION *)
+    player := Player.update_pokemon updated_pokemon !player;
+    create_ok_button dialog;
+(* 
+    printf "Do you want to add the wild %s to your team? (yes/no): " opponent.base.name;
+    match String.lowercase_ascii (read_line ()) with
+    | "yes" ->
+        let (new_player, was_added) = Player.add_team (Pokemon.copy_pokemon opponent) !player in
+        player := new_player;
+        if was_added then
+          printf "Added %s to your team.\n" opponent.base.name
+        else
+          printf "%s was not added to your team.\n" opponent.base.name
+    | "no" ->
+        printf "You chose not to add %s to your team.\n" opponent.base.name
+    | _ ->
+        let name = opponent.base.name in
+        printf "Invalid choice. Not adding %s to your team.\n" name *)
+  );
 
 
 and choose_starter () =
