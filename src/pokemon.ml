71dd9903279e801486387fe36d3fe4643e51c1f7
Akshay Aravind <akshayarav2013@gmail.com>
2023-12-10 18:45:49 -0500

    Remove unnecessary comments from pokedex.ml

--- src/pokemon.ml
+++ src/pokemon.ml
@@ -151,40 +151,33 @@ let string_to_ptype = function
   | "Fairy" -> Fairy
   | _ -> failwith "Unknown ptype"
 
-(** Creates a new pokemon *)
 let create base level =
   {
     base;
     hp = base.max_hp;
     current_max_hp = base.max_hp;
-    (* Initialize current max HP *)
     level;
     xp = (0, 10 * level);
     feint = false;
     damage_modifier = 1.;
   }
 
-(** Creates a new pokemon with a random hp, ensuring it's at least half of the max HP *)
 let create_random base level =
   let min_hp = base.max_hp / 2 in
-  (* Minimum HP is half of the max HP *)
   let random_hp = min_hp + Random.int (base.max_hp - min_hp + 1) in
   {
     base;
     hp = random_hp;
     current_max_hp = base.max_hp;
-    (* Initialize current max HP *)
     level;
     xp = (0, 10 * level);
     feint = false;
     damage_modifier = 1.;
   }
 
-(** Creates a deep copy of a pokemon *)
 let copy_pokemon p =
   { p with hp = p.hp; xp = (fst p.xp, snd p.xp); feint = p.feint }
 
-(** Pokemon's hp is updated when attacked *)
 let attack move opponent_pokemon player_pokemon =
   let effectiveness =
     match Hashtbl.find_opt type_effectiveness move.m_ptype with
@@ -213,7 +206,6 @@ let attack move opponent_pokemon player_pokemon =
     opponent_pokemon.feint <- true)
   else opponent_pokemon.hp <- new_hp
 
-(** Prints out pokemon and its stats *)
 let to_string pokemon =
   let feint_status = if pokemon.feint then " - FEINTED" else "" in
   let level_info =
@@ -228,10 +220,9 @@ let to_string pokemon =
   ^ ptype_to_string pokemon.base.ptype
   ^ ", " ^ level_info ^ feint_status
 
-(** Prints out the pokemon's name *)
 let name pokemon = pokemon.base.name
 
-let max_level = 100 (* Set the maximum level cap *)
+let max_level = 100 
 
 let level_up pokemon =
   let rec level_up_helper pokemon =
@@ -239,7 +230,6 @@ let level_up pokemon =
     if current_xp >= next_level_xp && pokemon.level < max_level then
       let new_level = pokemon.level + 1 in
       let new_max_hp = pokemon.current_max_hp + (new_level * 2) in
-      (* Updated to use current_max_hp *)
       let damage_increase = 0.1 in
 
       let updated_pokemon =
@@ -247,9 +237,7 @@ let level_up pokemon =
           pokemon with
           level = new_level;
           current_max_hp = new_max_hp;
-          (* Update current max HP *)
           hp = (if pokemon.hp > 0 then new_max_hp else pokemon.hp);
-          (* Heal if not feinted *)
           damage_modifier = pokemon.damage_modifier +. damage_increase;
           xp = (current_xp - next_level_xp, next_level_xp + (20 * new_level));
         }
@@ -259,21 +247,17 @@ let level_up pokemon =
   in
   level_up_helper pokemon
 
-(* Adds XP gained in victory *)
 let add_xp pokemon xp_earned =
   let updated_pokemon =
     { pokemon with xp = (fst pokemon.xp + xp_earned, snd pokemon.xp) }
   in
   level_up updated_pokemon
 
-(* Calculate experience gained from defeating an opponent *)
 let calculate_xp_gained opponent_level = 10 * opponent_level
 
-(*Checks for new moves for the pokemon*)
 let check_new_moves pokemon =
   List.filter (fun (lvl, _) -> lvl = pokemon.level) pokemon.base.learnable_moves
 
-(*Checks if the pokemon can evolve*)
 let check_evolution pokemon =
   match pokemon.base.evolution with
   | Some (lvl, evolved_form) when pokemon.level >= lvl -> Some evolved_form
@@ -282,7 +266,6 @@ let check_evolution pokemon =
 let evolve_pokemon pokemon evolved_form =
   { pokemon with base = evolved_form; hp = evolved_form.max_hp }
 
-(* Function to add or replace a move *)
 let learn_move pokemon new_move move_to_replace =
   let new_moves =
     List.mapi
