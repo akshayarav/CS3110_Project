c7de2c9c369fc26e5c40d18206f973f5373ee6a6
Hyungjun <hc867@cornell.edu>
2023-12-09 22:15:10 -0500

    Fixed redundancy in battle.ml
    - Removed redundant function in battle.ml
    - Adjusted testing using removed function

--- src/battle.ml
+++ src/battle.ml
@@ -1,43 +1,6 @@
 open Printf
 open Pokemon
 open Trainer
-open Player
-
-(** Allows the player to select a non-fainted Pokémon from their team. Returns
-    a tuple of the chosen Pokémon and an updated player record with
-    current_pokemon set to this Pokémon. *)
-let choose_new_pokemon player =
-  printf "Choose a new Pokemon:\n";
-  List.iteri
-    (fun i pokemon ->
-      if not pokemon.feint then
-        printf "%d. %s (HP: %d)\n" (i + 1) pokemon.base.name pokemon.hp)
-    player.team;
-
-  let rec get_pokemon_choice () =
-    try
-      let choice = read_int () in
-      if choice >= 1 && choice <= List.length player.team then
-        let chosen_pokemon = List.nth player.team (choice - 1) in
-        if not chosen_pokemon.feint then
-          Some chosen_pokemon
-        else (
-          printf "This Pokemon has fainted. Choose another one.\n";
-          get_pokemon_choice ())
-      else (
-        printf "Invalid choice. Please enter a valid number: ";
-        get_pokemon_choice ())
-    with
-    | Failure _ ->
-        printf "Invalid input. Please enter a valid number: ";
-        get_pokemon_choice ()
-    | _ ->
-        printf "An unexpected error occurred. Exiting the choice.\n";
-        raise Exit
-  in
-  match get_pokemon_choice () with
-  | Some chosen_pokemon -> (chosen_pokemon, { player with current_pokemon = Some chosen_pokemon })
-  | None -> failwith "No Pokemon was chosen"
 
 let on_move_chosen (move: move option) player_pokemon opponent update_ui next_turn_ui (result) =
   (* Here you would add the logic to apply the move, like reducing HP, etc. *)
@@ -67,8 +30,6 @@ let on_move_chosen (move: move option) player_pokemon opponent update_ui next_tu
     continue := false;
   );
   if !continue then next_turn_ui else ()
-
-  
 
 let update_trainer_team (trainer: trainer) (new_team: pokemon list) =
   { trainer with team = new_team }
