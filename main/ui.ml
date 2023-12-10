ebac7aa38e99cc8553be0e64982603da89113074
Hyungjun <hc867@cornell.edu>
2023-12-09 22:38:47 -0500

    Switched location of on_move_chosen appropriately for UI
    - Moved function on_move_chosen

--- main/ui.ml
+++ main/ui.ml
@@ -1,5 +1,6 @@
 open Mylib
 open Printf
+open Moves
 
 let main_menu : (GWindow.window option) ref = ref None
 
@@ -10,6 +11,34 @@ let get_main_menu () =
 
 let player = ref Player.new_player
 let starters_base = Pokedex.starters_base
+let on_move_chosen (move: move option) player_pokemon opponent update_ui next_turn_ui (result) =
+  (* Here you would add the logic to apply the move, like reducing HP, etc. *)
+  let process_move (move: move option) = 
+    match move with 
+    | Some chosen_move ->
+        update_ui (sprintf "%s used %s!\n" (Pokemon.name player_pokemon) chosen_move.name);
+        Pokemon.attack chosen_move opponent player_pokemon;  (* Apply player's chosen move *)
+        let ai_move = Ai.choose_move opponent in
+        update_ui (sprintf "%s used %s!\n" (Pokemon.name opponent) ai_move.name);
+        Pokemon.attack ai_move player_pokemon opponent;
+    | None -> failwith "Switching pokemon unimplemented"; 
+    in
+  process_move move;
+  update_ui (sprintf "\nYour %s's HP: %d\n" player_pokemon.base.name player_pokemon.hp);
+  update_ui (sprintf "%s's HP: %d\n" (Pokemon.name opponent) opponent.hp);
+  (* Check if the battle is over and handle accordingly *)
+  let continue = ref true in
+  if player_pokemon.hp <= 0 then (
+    update_ui "Your Pokemon fainted!";
+    result false;
+    continue := false;
+  )
+  else if opponent.hp <= 0 then (
+    update_ui (sprintf"%s defeated %s!" (Pokemon.name player_pokemon) (Pokemon.name opponent));
+    result true;
+    continue := false;
+  );
+  if !continue then next_turn_ui else ()
 
 (** Callback to update the UI during battles *)
 let update_ui_string dialog message =
@@ -76,7 +105,7 @@ let rec update_ui_moves dialog moves player_pokemon opponent result = (
       let button = GButton.button ~label:label_text ~packing:dialog#vbox#add () in
       ignore (
         button#connect#clicked ~callback:(fun () ->
-            Battle.on_move_chosen (Some move) player_pokemon opponent (update_ui_string dialog) (update_ui_moves dialog moves player_pokemon opponent result) result;
+          on_move_chosen (Some move) player_pokemon opponent (update_ui_string dialog) (update_ui_moves dialog moves player_pokemon opponent result) result;
         )
       );
     ) 
--- src/battle.ml
+++ src/battle.ml
@@ -1,35 +1,5 @@
-open Printf
 open Pokemon
 open Trainer
-
-let on_move_chosen (move: move option) player_pokemon opponent update_ui next_turn_ui (result) =
-  (* Here you would add the logic to apply the move, like reducing HP, etc. *)
-  let process_move (move: move option) = 
-    match move with 
-    | Some chosen_move ->
-        update_ui (sprintf "%s used %s!\n" (Pokemon.name player_pokemon) chosen_move.name);
-        Pokemon.attack chosen_move opponent player_pokemon;  (* Apply player's chosen move *)
-        let ai_move = Ai.choose_move opponent in
-        update_ui (sprintf "%s used %s!\n" (Pokemon.name opponent) ai_move.name);
-        Pokemon.attack ai_move player_pokemon opponent;
-    | None -> failwith "Switching pokemon unimplemented"; 
-    in
-  process_move move;
-  update_ui (sprintf "\nYour %s's HP: %d\n" player_pokemon.base.name player_pokemon.hp);
-  update_ui (sprintf "%s's HP: %d\n" (Pokemon.name opponent) opponent.hp);
-  (* Check if the battle is over and handle accordingly *)
-  let continue = ref true in
-  if player_pokemon.hp <= 0 then (
-    update_ui "Your Pokemon fainted!";
-    result false;
-    continue := false;
-  )
-  else if opponent.hp <= 0 then (
-    update_ui (sprintf"%s defeated %s!" (Pokemon.name player_pokemon) (Pokemon.name opponent));
-    result true;
-    continue := false;
-  );
-  if !continue then next_turn_ui else ()
 
 let update_trainer_team (trainer: trainer) (new_team: pokemon list) =
   { trainer with team = new_team }
