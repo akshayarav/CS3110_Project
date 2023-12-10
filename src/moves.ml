0283678f7e29806dae0e512c9a9f19e23fd551f7
Hyungjun <hc867@cornell.edu>
2023-12-10 10:33:32 -0500

    Eliminated Redundant Moves
    - Removed 0 attack moves due to stats effect difficulties

--- src/moves.ml
+++ src/moves.ml
@@ -1,9 +1,7 @@
 include Pokemon
 
 let tackle = { name = "Tackle"; damage = 10; m_ptype = Normal }
-let growl = { name = "Growl"; damage = 0; m_ptype = Normal }
 let scratch = { name = "Scratch"; damage = 10; m_ptype = Normal }
-let tail_whip = { name = "Tail Whip"; damage = 0; m_ptype = Normal }
 let ember = { name = "Ember"; damage = 10; m_ptype = Fire }
 let leech_seed = { name = "Leech Seed"; damage = 5; m_ptype = Grass }
 let vine_whip = { name = "Vine Whip"; damage = 10; m_ptype = Grass }
@@ -23,25 +21,19 @@ let hurricane = { name = "Hurricane"; damage = 20; m_ptype = Flying }
 let hyper_fang = { name = "Hyper Fang"; damage = 15; m_ptype = Normal }
 let drill_peck = { name = "Drill Peck"; damage = 20; m_ptype = Flying }
 let thunder_shock = { name = "Thunder Shock"; damage = 10; m_ptype = Electric }
-let thunder_wave = { name = "Thunder Wave"; damage = 0; m_ptype = Electric }
 let electro_ball = { name = "Electro Ball"; damage = 15; m_ptype = Electric }
 let thunder = { name = "Thunder"; damage = 20; m_ptype = Electric }
-let sing = { name = "Sing"; damage = 0; m_ptype = Normal }
 let pound = { name = "Pound"; damage = 10; m_ptype = Normal }
 let double_slap = { name = "Double Slap"; damage = 15; m_ptype = Normal }
-let rest = { name = "Rest"; damage = 0; m_ptype = Normal }
 let hyper_voice = { name = "Hyper Voice"; damage = 20; m_ptype = Normal }
 let confusion = { name = "Confusion"; damage = 10; m_ptype = Psychic }
 let fury_swipes = { name = "Fury Swipes"; damage = 15; m_ptype = Normal }
 let disable = { name = "Disable"; damage = 10; m_ptype = Normal }
-let sand_attack = { name = "Sand Attack"; damage = 0; m_ptype = Ground }
 let bulldoze = { name = "Bulldoze"; damage = 15; m_ptype = Ground }
 let dig = { name = "Dig"; damage = 40; m_ptype = Ground }
 let hyper_beam = { name = "Hyper Beam"; damage = 25; m_ptype = Normal }
 let future_sight = { name = "Future Sight"; damage = 20; m_ptype = Psychic }
 let extreme_speed = { name = "Extreme Speed"; damage = 20; m_ptype = Normal }
-let sleep_powder = { name = "Sleep Powder"; damage = 0; m_ptype = Grass }
-let poison_powder = { name = "Poison Powder"; damage = 0; m_ptype = Grass }
 let thunder_punch = { name = "Thunder Punch"; damage = 15; m_ptype = Electric }
 let ice_punch = { name = "Ice Punch"; damage = 15; m_ptype = Ice }
 let fire_punch = { name = "Fire Punch"; damage = 15; m_ptype = Fire }
@@ -53,16 +45,11 @@ let ice_beam = { name = "Ice Beam"; damage = 15; m_ptype = Ice }
 let blizzard = { name = "Blizzard"; damage = 20; m_ptype = Ice }
 let bite = { name = "Bite"; damage = 15; m_ptype = Dark }
 let dragon_rage = { name = "Dragon Rage"; damage = 40; m_ptype = Dragon }
-let leer = { name = "Leer"; damage = 0; m_ptype = Normal }
 let slam = { name = "Slam"; damage = 20; m_ptype = Normal }
 let psybeam = { name = "Psybeam"; damage = 15; m_ptype = Psychic }
-let reflect = { name = "Reflect"; damage = 0; m_ptype = Psychic }
-let recover = { name = "Recover"; damage = 0; m_ptype = Normal }
-let roar = { name = "Roar"; damage = 0; m_ptype = Normal }
 let take_down = { name = "Take Down"; damage = 25; m_ptype = Normal }
 let fly = { name = "Fly"; damage = 15; m_ptype = Flying }
 let stomp = { name = "Stomp"; damage = 10; m_ptype = Normal }
-let confuse_ray = { name = "Confuse Ray"; damage = 0; m_ptype = Ghost }
 let aurora_beam = { name = "Aurora Beam"; damage = 10; m_ptype = Ice }
 let wrap = { name = "Wrap"; damage = 5; m_ptype = Normal }
 let fake_out = { name = "Fake Out"; damage = 10; m_ptype = Normal }
@@ -72,12 +59,10 @@ let self_destruct = { name = "Self-Destruct"; damage = 50; m_ptype = Normal }
 let mirror_shot = { name = "Mirror Shot"; damage = 15; m_ptype = Steel }
 let bone_club = { name = "Bone Club"; damage = 15; m_ptype = Ground }
 let headbutt = { name = "Headbutt"; damage = 15; m_ptype = Normal }
-let focus_energy = { name = "Focus Energy"; damage = 0; m_ptype = Normal }
 let bonemerang = { name = "Bonemerang"; damage = 20; m_ptype = Ground }
 let horn_attack = { name = "Horn Attack"; damage = 15; m_ptype = Normal }
 let fury_attack = { name = "Fury Attack"; damage = 5; m_ptype = Normal }
 let leech_life = { name = "Leech Life"; damage = 10; m_ptype = Bug }
-let spore = { name = "Spore"; damage = 0; m_ptype = Grass }
 let slash = { name = "Slash"; damage = 20; m_ptype = Normal }
 let spark = { name = "Spark"; damage = 20; m_ptype = Electric; }
 let thunderbolt = { name = "Thunderbolt"; damage = 25; m_ptype = Electric; }
@@ -87,36 +72,23 @@ let mud_slap = { name = "Mud-Slap"; damage = 10; m_ptype = Ground; }
 let bone_rush = { name = "Bone Rush"; damage = 25; m_ptype = Ground }
 let double_edge = { name = "Double-Edge"; damage = 30; m_ptype = Normal }
 let rock_slide = { name = "Rock Slide"; damage = 20; m_ptype = Rock }
-let stun_spore = { name = "Stun Spore"; damage = 0; m_ptype = Grass }
 let giga_drain = { name = "Giga Drain"; damage = 15; m_ptype = Grass; }
-let harden = { name = "Harden"; damage = 0; m_ptype = Normal }
-let string_shot = { name = "String Shot"; damage = 0; m_ptype = Bug }
 let bug_bite = { name = "Bug Bite"; damage = 10; m_ptype = Bug }
 let poison_sting = { name = "Poison Sting"; damage = 15; m_ptype = Poison }
 let swift = { name = "Swift"; damage = 20; m_ptype = Normal }
-let barrier = { name = "Barrier"; damage = 0; m_ptype = Psychic }
-let amnesia = { name = "Amnesia"; damage = 0; m_ptype = Psychic }
-let mist = { name = "Mist"; damage = 0; m_ptype = Ice }
 let sky_attack = { name = "Sky Attack"; damage = 50; m_ptype = Flying }
-let agility = { name = "Agility"; damage = 0; m_ptype = Psychic }
-let light_screen = { name = "Light Screen"; damage = 0; m_ptype = Psychic }
 let fire_spin = { name = "Fire Spin"; damage = 35; m_ptype = Fire }
 let aeroblast = { name = "Aeroblast"; damage = 100; m_ptype = Flying }
 let sacred_fire = { name = "Sacred Fire"; damage = 100; m_ptype = Fire }
-let sunny_day = { name = "Sunny Day"; damage = 0; m_ptype = Fire }
 let outrage = { name = "Outrage"; damage = 120; m_ptype = Dragon }
 let water_spout = { name = "Water Spout"; damage = 150; m_ptype = Water }
 let ancient_power = { name = "Ancient Power"; damage = 60; m_ptype = Rock }
-let sheer_cold = { name = "Sheer Cold"; damage = 0; m_ptype = Ice }
 let eruption = { name = "Eruption"; damage = 150; m_ptype = Fire }
-let fissure = { name = "Fissure"; damage = 0; m_ptype = Ground }
 let get_chicks = { name = "Get Chicks"; damage = 999; m_ptype = Ground }
 let all_moves =
   [
     tackle;
-    growl;
     scratch;
-    tail_whip;
     ember;
     leech_seed;
     vine_whip;
@@ -136,21 +108,15 @@ let all_moves =
     hyper_fang;
     drill_peck;
     thunder_shock;
-    thunder_wave;
     electro_ball;
     thunder;
-    sing;
     pound;
     double_slap;
-    rest;
     hyper_voice;
     confusion;
     fury_swipes;
-    sand_attack;
     bulldoze;
     dig;
-    sleep_powder;
-    poison_powder;
     thunder_punch;
     ice_punch;
     fire_punch;
@@ -163,15 +129,11 @@ let all_moves =
     hyper_beam;
     bite;
     dragon_rage;
-    leer;
     slam;
     psybeam;
-    recover;
-    roar;
     take_down;
     fly;
     stomp;
-    confuse_ray;
     aurora_beam;
     wrap;
     fake_out;
@@ -181,40 +143,27 @@ let all_moves =
     mirror_shot;
     bone_club;
     headbutt;
-    focus_energy;
     bonemerang;
     horn_attack;
     fury_attack;
     leech_life;
-    spore;
     slash;
     spark;
     thunderbolt;
     shock_wave;
     mud_slap;
     rock_slide;
-    stun_spore;
     giga_drain;
-    harden;
-    string_shot;
     bug_bite;
     poison_sting;
     swift;
-    barrier;
-    amnesia;
-    mist;
     sky_attack;
-    agility;
-    light_screen;
     fire_spin;
     aeroblast;
     sacred_fire;
-    sunny_day;
     outrage;
     water_spout;
     ancient_power;
-    sheer_cold;
     eruption;
-    fissure;
     get_chicks;
   ]
